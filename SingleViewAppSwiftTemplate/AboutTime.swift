d9c0505aef739d311a4d1613a64b9fef136b5211
Jason Vest <jason@jasonvest.com>
2019-06-23 17:21:45 -0700

    Auto Layout and update model
    Performed auto layout on the labels and buttons.  Completed first pass at model work, ui work to reorder events

--- SingleViewAppSwiftTemplate/AboutTime.swift
+++ SingleViewAppSwiftTemplate/AboutTime.swift
@@ -7,19 +7,135 @@
 //
 
 import Foundation
+import GameKit
 
-enum EventSelection {
+enum EventGroup: String {
+    case WorldWarIIEvents
+}
+
+enum EventSelection: String {
     case event
     case date
 }
 
+enum EventError: Error  {
+    case invalidResource
+    case conversionError
+}
+
 protocol HistoricalEvent {
-    var eventDate: Date { get }
     var eventDescription: String { get }
+    var eventDate: Date { get }
+    //var wikipediaURL: URL { get }
 }
 
 protocol TimelineQuiz   {
+    var events: [HistoricalEvent] { get set }
+}
+
+protocol QuizManager    {
+    var numberOfRounds: Int { get set }
+    var roundsUsed: Int { get set }
+    var numberOfEvents: Int { get set }
+    var roundLength: Int { get set }
+    var numberOfCorrectRounds: Int { get set }
+    var quiz: TimelineQuiz { get set }
+    var eventSet: [HistoricalEvent] { get set }
+    
+    init(numberOfRounds: Int, numberOfEvents: Int, roundLength: Int, events: [HistoricalEvent])
+    func isGameOver() -> Bool
+    func resetGame() -> Void
+    //func checkAnswer(listOf events: [HistoricalEvent], isTimeUp timeUp: Bool) -> Bool
+    func getRandomEvents() -> Void
+    func changeEventOrder(firstPosition: Int, secondPostion: Int) -> Void
     
 }
 
+struct Event: HistoricalEvent    {
+    let eventDescription: String
+    let eventDate: Date
+    //let wikipediaURL: URL
+}
 
+class PlistConverter    {
+    static func dictionary(fromFile name: String, ofType type: String) throws -> [String : AnyObject]    {
+        guard let path = Bundle.main.path(forResource: name, ofType: type) else {
+            throw EventError.invalidResource
+        }
+        guard let dictionary = NSDictionary(contentsOfFile: path) as? [String : AnyObject] else {
+            throw EventError.conversionError
+        }
+        return dictionary
+    }
+}
+
+class EventLoader   {
+    static func quizEvents(fromDictionary dictionary: [String : AnyObject]) throws -> [HistoricalEvent] {
+        var eventsList: [HistoricalEvent] = []
+        
+        for (_, value) in dictionary  {
+            if let eventDictionary = value as? [String : Any], let eventDescription = eventDictionary["event"] as? String, let eventDate = eventDictionary["date"] as? Date {
+                let event = Event(eventDescription: eventDescription, eventDate: eventDate)
+                
+                eventsList.append(event)
+            }
+        }
+        return eventsList
+    }
+}
+
+class WorldWarIIQuiz: TimelineQuiz  {
+    var events: [HistoricalEvent]
+    
+    init(events: [HistoricalEvent]) {
+        self.events = events
+    }
+}
+
+class WorldWarIIQuizManager: QuizManager    {
+    var numberOfRounds: Int
+    var roundsUsed: Int = 0
+    var numberOfEvents: Int
+    var roundLength: Int
+    var numberOfCorrectRounds: Int = 0
+    var quiz: TimelineQuiz
+    var eventSet: [HistoricalEvent] = []
+    
+    required init(numberOfRounds: Int, numberOfEvents: Int, roundLength: Int, events: [HistoricalEvent]) {
+        self.numberOfRounds = numberOfRounds
+        self.numberOfEvents = numberOfEvents
+        self.roundLength = roundLength
+        self.quiz = WorldWarIIQuiz.init(events: events)
+        self.getRandomEvents()
+    }
+    
+    func isGameOver() -> Bool {
+        if self.roundsUsed == self.numberOfRounds  {
+            return true
+        } else  {
+            return false
+        }
+    }
+    
+    func resetGame() {
+        self.roundsUsed = 0
+        self.numberOfCorrectRounds = 0
+        self.eventSet = []
+    }
+    /*
+    func checkAnswer(listOf events: [HistoricalEvent], isTimeUp timeUp: Bool) -> Bool {
+     
+    }*/
+    
+    func getRandomEvents() -> Void  {
+        repeat  {
+            let index = GKRandomSource.sharedRandom().nextInt(upperBound: self.quiz.events.count)
+            eventSet.append(self.quiz.events[index])
+        } while eventSet.count < self.numberOfEvents
+    }
+    
+    func changeEventOrder(firstPosition: Int, secondPostion: Int) {
+        self.eventSet.swapAt(firstPosition, secondPostion)
+    }
+    
+}
--- SingleViewAppSwiftTemplate/ViewController.swift
+++ SingleViewAppSwiftTemplate/ViewController.swift
@@ -9,10 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var firstEventLabel: UILabel!
+    @IBOutlet weak var secondEventLabel: UILabel!
+    @IBOutlet weak var thirdEventLabel: UILabel!
+    @IBOutlet weak var fourthEventLabel: UILabel!
+    @IBOutlet weak var firstEventDownButton: UIButton!
+    @IBOutlet weak var secondEventUpButton: UIButton!
+    @IBOutlet weak var secondEventDownButton: UIButton!
+    @IBOutlet weak var thirdEventUpButton: UIButton!
+    @IBOutlet weak var thirdEventDownButton: UIButton!
+    @IBOutlet weak var fourthEventUpButton: UIButton!
+    
+    
+    let numberOfRounds = 6
+    let numberOfEvents = 4
+    let roundLength = 60
+    let quizManager: QuizManager
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle  {
+        return .lightContent
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        do  {
+            let dictionary = try PlistConverter.dictionary(fromFile: EventGroup.WorldWarIIEvents.rawValue, ofType: "plist")
+            
+            let eventsList = try EventLoader.quizEvents(fromDictionary: dictionary)
+            
+            self.quizManager = WorldWarIIQuizManager.init(numberOfRounds: numberOfRounds, numberOfEvents: numberOfEvents, roundLength: roundLength, events: eventsList)
+        } catch let error   {
+            fatalError("\(error)")
+        }
+        super.init(coder: aDecoder)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Populate labels with initial set of events
+        populateLables()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +57,70 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func labelArray() -> [UILabel]  {
+        let labelSet: [UILabel] = [firstEventLabel,
+                                   secondEventLabel,
+                                   thirdEventLabel,
+                                   fourthEventLabel]
+        return labelSet
+    }
+    
+    func buttonArray() -> [UIButton]  {
+        let buttonSet: [UIButton] = [firstEventDownButton,
+                                     secondEventUpButton,
+                                     secondEventDownButton,
+                                     thirdEventUpButton,
+                                     thirdEventDownButton,
+                                     fourthEventUpButton]
+        return buttonSet
+    }
+    func populateLables() -> Void   {
+        let labelSet = labelArray()
+        for index in 0..<labelSet.count   {
+            labelSet[index].text = quizManager.eventSet[index].eventDescription
+        }
+    }
+    
+    @IBAction func reorderEvents(_ sender: UIButton) {
+        var firstPosition: Int = 0
+        var secondPosition: Int = 0
+        if sender === firstEventDownButton  {
+            firstPosition = 0
+            secondPosition = 1
+        } else if sender === secondEventUpButton    {
+            firstPosition = 1
+            secondPosition = 0
+        } else if sender === secondEventDownButton  {
+            firstPosition = 1
+            secondPosition = 2
+        } else if sender === thirdEventUpButton {
+            firstPosition = 2
+            secondPosition = 1
+        } else if sender === thirdEventDownButton   {
+            firstPosition = 2
+            secondPosition = 3
+        } else if sender === fourthEventUpButton    {
+            firstPosition = 3
+            secondPosition = 2
+        }
+        if firstPosition != secondPosition  {
+            quizManager.changeEventOrder(firstPosition: firstPosition, secondPostion: secondPosition)
+            populateLables()
+        }
+    }
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if sender === firstEventDownButton  {
+            sender.setImage(UIImage(named: "down_full_selected.png"), for: .highlighted)
+        } else if sender === secondEventUpButton || sender === thirdEventUpButton    {
+            sender.setImage(UIImage(named: "up_half_selected.png"), for: .highlighted)
+        } else if sender === secondEventDownButton || sender === thirdEventDownButton   {
+            sender.setImage(UIImage(named: "down_half_selected.png"), for: .highlighted)
+        } else if sender === fourthEventUpButton    {
+            sender.setImage(UIImage(named: "up_full_selected.png"), for: .highlighted)
+        }
+    }
+    
 }
 
